57269127a8fcdd07d1e68248a957949731a909cb
Martin Pärtel <martin.partel@gmail.com>
2011-07-25 19:41:22 +0300

    Decided to make module includable.

--- ext/ruby_php/ruby_php.c
+++ ext/ruby_php/ruby_php.c
@@ -7,7 +7,7 @@ static VALUE hello_world(VALUE mod);
 void Init_ruby_php()
 {
   VALUE module = rb_define_module("PHP");
-  rb_define_singleton_method(module, "hello_world", hello_world, 0);
+  rb_define_method(module, "hello_world", hello_world, 0);
 }
 
 static VALUE hello_world(VALUE mod)
--- lib/ruby_php.rb
+++ lib/ruby_php.rb
@@ -2,5 +2,17 @@ require 'ruby_php/ruby_php'
 
 module PHP
   VERSION = '0.0.1'
+  
+  @default_instance = Proc.new do
+    obj = Object.new
+    class << obj
+      include PHP
+    end
+    obj
+  end.call
+  
+  def self.method_missing(*args)
+    @default_instance.send(*args)
+  end
 end
 
